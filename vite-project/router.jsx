c1514af3499ccf187ae4a2ee5aa5997db2031dc8
ABHIJITHANU <abhijithsr.personal@gmail.com>
2023-10-27 18:02:33 +0530

    created routes and its components

---
+++ vite-project/router.jsx
@@ -0,0 +1,49 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Login from "./src/views/Login";
+import Signup from "./src/views/Signup";
+import Users from "./src/views/Users";
+import NotFound from "./src/views/NotFound";
+import DefaultLayout from "./src/components/DefaultLayout";
+import GuestLayout from "./src/components/GuestLayout";
+import Dashboard from "./src/views/Dashboard";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Navigate to="/users"/>
+            },
+            {
+                path: '/users',
+                element: <Users />
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            },
+        ]
+    },
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+        ]
+    },
+    {
+        path: "*",
+        element: <NotFound />
+    }
+])
+
+export default router;
