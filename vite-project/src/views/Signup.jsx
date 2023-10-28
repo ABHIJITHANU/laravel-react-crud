937248baa0ed47d9c0a620e90535b142ebdc16c7
ABHIJITHANU <abhijithsr.personal@gmail.com>
2023-10-29 00:02:15 +0530

    login and signup api implementation and server validation mesages display

--- vite-project/src/views/Signup.jsx
+++ vite-project/src/views/Signup.jsx
@@ -1,18 +1,60 @@
 import { Link } from "react-router-dom";
+import axiosClient from "../asios-client";
+import { useStateContext } from "../contexts/ContextProvider";
+import { useRef, useState } from "react";
 
 function Signup() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfRef = useRef();
+  const [errors, setErrors] = useState(null);
+  const { setUser, setToken } = useStateContext();
+
   const onSubmit = (event) => {
     event.preventDefault();
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfRef.current.value,
+    };
+    setErrors(null);
+    axiosClient
+      .post("/signup", payload)
+      .then(({ data }) => {
+        setUser(data.user);
+        setToken(data.token);
+      })
+      .catch((err) => {
+        const response = err.response;
+        if (response && response.status === 422) {
+          setErrors(response.data.errors);
+        }
+      });
   };
   return (
     <div className="login-signup-form animated fadeInDown">
       <div className="form">
         <form onSubmit={onSubmit}>
           <h1 className="title">Signup your account</h1>
-          <input type="fullname" placeholder="Full Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Password Confirmation" />
+
+          {errors && (
+            <div className="alert">
+              {Object.keys(errors).map((key) => (
+                <p key={key}>{errors[key][0]}</p>
+              ))}
+            </div>
+          )}
+
+          <input ref={nameRef} placeholder="Full Name" />
+          <input ref={emailRef} type="email" placeholder="Email" />
+          <input ref={passwordRef} type="password" placeholder="Password" />
+          <input
+            ref={passwordConfRef}
+            type="password"
+            placeholder="Password Confirmation"
+          />
           <button className="btn btn-block">Signup</button>
           <p className="message">
             Already registered? <Link to="/login">Login</Link>
@@ -21,7 +63,6 @@ function Signup() {
       </div>
     </div>
   );
-  }
-  
-  export default Signup;
-  
+}
+
+export default Signup;
