87d248c81780d197b76c5f86bd72e4ca062d6c3a
ABHIJITHANU <abhijithsr.personal@gmail.com>
2023-10-28 20:46:58 +0530

    axios installation and login register form design not functionality

--- vite-project/src/components/DefaultLayout.jsx
+++ vite-project/src/components/DefaultLayout.jsx
@@ -1,14 +1,34 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
 
 function DefaultLayout() {
-  const { token } = useStateContext();
+  const { user, token } = useStateContext();
   if (!token) {
     return <Navigate to="/login" />;
   }
+  const onLogout = (event) => {
+    event.preventDefault();
+  };
   return (
-    <div>
-      <Outlet />
+    <div id="defaultLayout">
+      <aside>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/users">Users</Link>
+      </aside>
+      <div className="content">
+        <header>
+          <div>Header</div>
+          <div>
+            {user.name}
+            <a href="#" onClick={onLogout} className="btn-logout">
+              Logout
+            </a>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
--- vite-project/src/views/Signup.jsx
+++ vite-project/src/views/Signup.jsx
@@ -1,9 +1,26 @@
+import { Link } from "react-router-dom";
+
 function Signup() {
-    return (
-      <div>
-          Signup
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className="title">Signup your account</h1>
+          <input type="fullname" placeholder="Full Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Password Confirmation" />
+          <button className="btn btn-block">Signup</button>
+          <p className="message">
+            Already registered? <Link to="/login">Login</Link>
+          </p>
+        </form>
       </div>
-    )
+    </div>
+  );
   }
   
   export default Signup;
